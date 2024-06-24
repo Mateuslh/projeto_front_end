3e5feb1aa6cf3a276b527c96ae09b2ec080069fa
pauloFrancisconi <pauloffrancisconi@gmail.com>
2024-06-24 14:52:50 -0300

    [Fix] - Reestilização 2 Mudança das cores da navbar, além de mais um fix no footer, ele sempre vai aparecer no final do conteúdo da página e sempre será forçado para o final da página também

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/cabecalho-rodape/navbar.jsx';
@@ -9,24 +8,26 @@ import Login from './pages/Login';
 import { UserProvider } from './components/UserContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BuscarDebitos from "./components/contribuinte/tabDebitosContrib.jsx";
-import './App.css'
+import './App.css';
 import Footer from './components/cabecalho-rodape/footer.jsx';
 
 const App = () => {
     return (
         <UserProvider>
             <Router>
-                <div>
-                    <Navbar /> 
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/home" element={<Home />} /> 
-                        <Route path="/contribuintes" element={<Contribuintes />} /> 
-                        <Route path="/debitos" element={<Debitos />} />
-                        <Route path="/contribuinte/:id/getDebitos" component={BuscarDebitos} />
-                        <Route path="*" element={<Navigate to="/login" />} />
-                    </Routes>
-                    <Footer/>
+                <div className="d-flex flex-column min-vh-100">
+                    <Navbar />
+                    <div className="flex-grow-1">
+                        <Routes>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/contribuintes" element={<Contribuintes />} />
+                            <Route path="/debitos" element={<Debitos />} />
+                            <Route path="/contribuinte/:id/getDebitos" element={<BuscarDebitos />} />
+                            <Route path="*" element={<Navigate to="/login" />} />
+                        </Routes>
+                    </div>
+                    <Footer />
                 </div>
             </Router>
         </UserProvider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Saudacao from '../components/utils/saudacao.jsx';
 import Dashboard from '../components/dashboard/dashboard.jsx';
 
-
 const Home = () => {
     return (
-        <div className="container mt-5">
+        <div className="container mt-5 mb-5">
             <div className="row">
                 <div className="col">
                     <Saudacao />
                     <p>Bem-vindo à página inicial.</p>
-                    <Dashboard/>
+                    <Dashboard />
                 </div>
             </div>
         </div>
@@ -18,4 +17,5 @@ const Home = () => {
 };
 
 export default Home;
+
 
